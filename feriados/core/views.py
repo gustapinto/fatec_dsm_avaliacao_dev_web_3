126a34de436225ac6714ed94c70f7066fe8b529b
Caroliny França <francinha.caroliny@gmail.com>
2022-06-01 21:24:30 -0300

    Adicionadas funcionalidades na view

--- feriados/core/views.py
+++ feriados/core/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render, HttpResponse
+from datetime import date
+from django.shortcuts import render
+from cadastro_api.models import FeriadoModel
 
-def verificar(request): 
-    return HttpResponse('')
+def verificar(request):
+    hoje = date.today()
+
+    try:
+        feriado = FeriadoModel.objects.get(data=hoje)
+    except FeriadoModel.DoesNotExist:
+        feriado = None
+
+    mensagem = f'Hoje é {feriado}' if feriado else 'Hoje não é feriado'
+
+    return render(request, 'feriados.html', {'mensagem': mensagem})
