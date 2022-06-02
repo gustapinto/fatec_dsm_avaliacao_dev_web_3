2785cdaa4e396780591d2a3a2e222245f8e97c09
Caroliny França <francinha.caroliny@gmail.com>
2022-06-01 21:10:35 -0300

    Criada a view do core

--- feriados/feriados/urls.py
+++ feriados/feriados/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
+    path('', include('core.urls')),
     path('cadastroAPI/', include('cadastro_api.urls')),
 ]
