04c12c674fe8d68dea5c046285d8e31efbd40ab8
Gustavo <ghp2201@gmail.com>
2022-06-08 20:01:25 -0300

    Add cadstro_feriados test

--- feriados/cadastro_feriado/tests.py
+++ feriados/cadastro_feriado/tests.py
@@ -1,3 +1,14 @@
+from http import HTTPStatus
+
 from django.test import TestCase
 
-# Create your tests here.
+
+class FeriadoFormTest(TestCase):
+    def setUp(self):
+        self.mockedData = {'nome': 'foo', 'data': '2022-04-02'}
+
+    def test_post_sucesso(self):
+        r = self.client.post('/cadastro/', data=self.mockedData)
+
+        self.assertEqual(r.status_code, HTTPStatus.OK)
+        self.assertContains(r, 'cadastrado com sucesso!')
